3166c5f68106a93c01ccd683b8873d2522c21f9a
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-09-18 20:53:22 +0300

    Added Walls script

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite; // sprite to display once player hits the wall
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+    /// <summary>
+    /// Deal damage to wall
+    /// </summary>
+    /// <param name="loss">amount of damage</param>
+    public void DamageWall(int loss)
+    {
+        spriteRenderer.sprite = dmgSprite; // visual feedback
+        hp -= loss;
+        if (hp <= 0) // if wall has no hp left then destroy it!
+            gameObject.SetActive(false);
+    }
+}
