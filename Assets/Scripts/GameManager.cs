0511b09dd0bbe5e9f2bbab66ba2048abe85af68f
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-09-18 19:37:06 +0300

    Implemented GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null; // singleton instance holder
+    public BoardManager boardScript;
+    private int level = 1; // test level 3
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        if (instance == null) // if there is no instance...
+            instance = this; // assign self to it
+        else if (instance != this) // if self is not instance...
+            Destroy(gameObject); // destroy it!
 
+        DontDestroyOnLoad(gameObject); // to keep data between scenes
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
